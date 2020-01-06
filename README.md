12f7ac37dea7c79575d9a7ee1e25ed0babc772e5
Sanjay Narayana <sanjay.narayana@hotmail.com>
2020-01-06 15:20:27 +1100

    add YEAR command line argument; updated docs

--- README.md
+++ README.md
@@ -4,13 +4,39 @@
 
 I was inspired by this post on [HackerNews](https://news.ycombinator.com/item?id=21921165) to re-create a terminal version of the same calendar.
 
-I have transposed the original design as I felt having the numbers on top makes it more readable. Along with the months on the side as reading left-to-right is more comprehensible for the human eye (atleast from a Westerner's perspective).
+I have transposed the original design to improve readability. I felt having the numbers on top and months on the side (reading left-to-right)made it readable.
 
-At the moment, it's a simple Python script that runs and prints to stdout. The pixel calculations are all magic numbers that needs to be generalised. See the wishlist below, I will cross off tasks as I work through them.
+This is a simple Python script that prints to stdout. Pixel calculations are all magic numbers that needs to be generalised. See the wishlist below, I will cross off tasks as I work through them.
 
 ## Usage
 
 Make sure you are running Python 3.6+
+
+```plain
+Usage:  python one_page_calendar.py [YEAR]
+```
+
+You can specify what YEAR you want (e.g. 2009).
+
+```plain
+$ python one_page_calendar.py 2009
+                  2009
+               1   2   3   4   5   6   7
+               8   9  10  11  12  13  14
+              15  16  17  18  19  20  21
+              22  23  24  25  26  27  28
+              29  30  31
+            ┌----------------------------
+        Jun | Mon Tue Wed Thu Fri Sat Sun
+    Sep Dec | Tue Wed Thu Fri Sat Sun Mon
+    Apr Jul | Wed Thu Fri Sat Sun Mon Tue
+    Jan Oct | Thu Fri Sat Sun Mon Tue Wed
+        May | Fri Sat Sun Mon Tue Wed Thu
+        Aug | Sat Sun Mon Tue Wed Thu Fri
+Feb Mar Nov | Sun Mon Tue Wed Thu Fri Sat
+```
+
+If no year is specified, the current year is used.
 
 ```plain
 $ python one_page_calendar.py
@@ -29,6 +55,8 @@ Jan Apr Jul | Wed Thu Fri Sat Sun Mon Tue
     Feb Aug | Sat Sun Mon Tue Wed Thu Fri
     Mar Nov | Sun Mon Tue Wed Thu Fri Sat
 ```
+
+---
 
 ## How to read the one page calendar
 
--- one_page_calendar.py
+++ one_page_calendar.py
@@ -1,46 +1,56 @@
 from calendar import Calendar, day_abbr, month_abbr
 from collections import defaultdict
 import time
+import sys
 
-curr_time = time.localtime(time.time())
-year = curr_time.tm_year
+# Read command-line arguments
+try:
+    year = int(sys.argv[1])
+except IndexError:
+    # No year specified.
+    # Default to current year
+    curr_time = time.localtime(time.time())
+    year = curr_time.tm_year
+except ValueError:
+    sys.stderr.write(f"Error! {sys.argv[1]} is not a valid year.\n")
+    sys.stderr.write(f"Usage:\tpython {sys.argv[0]} [YEAR]\n")
+    sys.exit(1)
 
-# Initialise a bucket mapping
-# the day to the month w/ the first
-# day
+# Init a bucket which maps day to 
+# the month w/ the first day
 firstday2month = defaultdict(list)
 
+# Find the day of every month's 1st.
 c = Calendar()
-# Find the date of any given
 for month in range(1, len(month_abbr)):
     for week in c.monthdays2calendar(year, month):
         for day_num, day in week:
-            # place the month into the day bucket
-            # e.g. if the 1st of Feb falls on a Saturday
+            # Place the month into the day bucket
+            # e.g. if the Feb 1st falls on a Saturday
             # then place Feb into the Sat bucket
             if day_num == 1:
-                firstday2month[day].append(month);
+                firstday2month[day].append(month)
                 break
                 
-                
-
+# Calculate the left padding
+# for printing the day grid below
 max_months = max([len(m) for m in firstday2month.values()])
 lpad = (3 * max_months) + 2
 
-# print out year number
+# Print out year number
 print(str(year).center(39))
 
-# print out day numbers
+# Print out day numbers
 for i in range(1, 32):
     print(f"{''.ljust(lpad+1, ' ')}", end=' ') if i % 7 == 1 else None
     print(f"{str(i).rjust(3, ' ')}", end=' ')
     print() if i % 7 == 0 else None
 print()
 
-vsep = ''.join([' '.ljust(lpad+1), '┌', '-'*28])
-print(vsep)
+# Print horizontal sep of dashes
+print(''.join([' '.ljust(lpad+1), '┌', '-'*28]))
 
-# print out months | days
+# Print out months | days
 for i in range(len(day_abbr)):
     mx = ' '.join([month_abbr[m] for m in firstday2month[i]])
     print(f"{mx.rjust(lpad, ' ')}", end=' | ')
